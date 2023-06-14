e99e9cdc6d5cd5c264f197ac15435bb34fee89ea
Oleg Khorev <o.g.khorev@gmail.com>
2023-06-14 20:08:01 +0300

    task 15

--- Homeworks/Homework2/Program.cs
+++ Homeworks/Homework2/Program.cs
@@ -22,3 +22,73 @@
 // SecondNum(num);
 
 // ----------------------------------------------------------------
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void ThirdNum (int[] num)
+{
+    int Length = num.Length;
+    int index = 2;
+    if(Length - 1 >= 2)
+    {
+        System.Console.WriteLine($"цифра {num[index]} является второй цифрой числа {num}");
+    }
+    else
+    {
+        Console.WriteLine("В числе {num} третьей цифры нет");
+    }
+
+}
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ThirdNum(num);
+
+// ----------------------------------------------------------------
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// ------ решение методом bool -------
+
+// bool WeekendNum (int num)
+// {
+//     if (num == 6 || num == 7) return true;
+//     else return false;
+// }
+
+// System.Console.Write("Введите цифру, обозначающую день недели -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(WeekendNum(num));
+
+// ------ решение методом void (с обозначением дней недели)------
+
+// void WeekendNum(int num)
+// {
+//     if (num == 6)
+//     {
+//         Console.WriteLine($"Цифра {num} обозначает субботу. Суббота является выходным днем.");
+//     }
+//     else if (num == 7)
+//     {
+//         Console.WriteLine($"Цифра {num} обозначает воскресенье. Воскресенье является выходным днем.");
+//     }
+//     else if (num >= 1 && num <= 6)
+//     {
+//         Console.WriteLine($"Цифра {num} обозначает будний день и не является выходным днем.");
+//     }
+//     else
+//         Console.WriteLine($"Цифра (число) {num} не является обозначением дня недели. Повторите запрос.");
+// }
+
+// System.Console.Write("Введите цифру, обозначающую день недели -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// WeekendNum(num);
