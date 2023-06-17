5c3b27068da143479924514715e0f9ab258ed7d3
Oleg Khorev <o.g.khorev@gmail.com>
2023-06-17 15:38:40 +0300

    task 21

--- Homeworks/Homework3/Program.cs
+++ Homeworks/Homework3/Program.cs
@@ -13,6 +13,25 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// double FindDistance(double xa, double ya, double xb, double yb, double za, double zb)
+// {
+//     return Math.Round(Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2)), 2);
+// }
+
+// System.Console.WriteLine("input X coordinate point A: ");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine("input Y coordinate point A: ");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine("input Z coordinate point A: ");
+// double za = Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine("input X coordinate point B: ");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine("input Y coordinate point B: ");
+// double yb = Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine("input Z coordinate point B: ");
+// double zb = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine($"Distance between points A and B -> {FindDistance(xa, ya, xb, yb, za, zb)}");
 
 
 // --------------------------------------------------------------------------------------------------
