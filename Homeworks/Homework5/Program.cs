6cae26a5e3631b5a0dcb5001c53a83fd0664a301
Oleg Khorev <o.g.khorev@gmail.com>
2023-06-24 16:38:47 +0300

    task 36

--- Homeworks/Homework5/Program.cs
+++ Homeworks/Homework5/Program.cs
@@ -2,12 +2,58 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// int[] CreateRandomArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+// }
+
+// int EvenNumbers(int[] array)
+// {
+//     int current = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             current++;
+//         }
+//     }
+//     return current;
+// }
+
+// System.Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = CreateRandomArray(size);
+
+// System.Console.Write($"In the array: ");
+// PrintArray(array);
+// System.Console.Write($"the number of even numbers -> {EvenNumbers(array)}");
+
+// ------------------------------------------------------------------------------------------------
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(100, 1000);
+        array[i] = new Random().Next(minValue, maxValue + 1);
     }
     return array;
 }
@@ -21,37 +67,31 @@ void PrintArray(int[] array)
     System.Console.WriteLine();
 }
 
-int EvenNumbers(int[] array)
+int SumOddPos(int[] array)
 {
-    int current = 0;
+    int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0)
+        if (i % 2 != 0)
         {
-            current++;
+            sum += array[i];
         }
     }
-    return current;
+    return sum;
 }
 
 System.Console.WriteLine("Input array size: ");
 int size = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Enter the minimum value of a three-digit array element [100 to 999]:");
+System.Console.WriteLine("Input minimal value of array element: ");
 int minValue = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Enter the maximum value of a three-digit array element [100 to 999]:");
+System.Console.WriteLine("Input maximal value of array element: ");
 int maxValue = Convert.ToInt32(Console.ReadLine());
+
 int[] array = CreateRandomArray(size, minValue, maxValue);
 
-System.Console.WriteLine($"In the array:");
+System.Console.Write($"In the array: ");
 PrintArray(array);
-System.Console.Write($"{EvenNumbers(array)} even numbers");
-
-// ------------------------------------------------------------------------------------------------
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-
+System.Console.Write($"the sum of the elements standing in odd positions is equal to {SumOddPos(array)}");
 
 // ------------------------------------------------------------------------------------------------
 
