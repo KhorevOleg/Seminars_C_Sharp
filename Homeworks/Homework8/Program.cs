0251a328352b148118ee0034eb1c76e008fd2b57
Oleg Khorev <o.g.khorev@gmail.com>
2023-07-06 01:51:44 +0300

    task54

---
+++ Homeworks/Homework8/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] Create2DArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] Ordering2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            if (array[i, j] < array[i, j + 1])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, j + 1];
+                array[i, j + 1] = temp;
+                i = 0;
+                j = -1;
+            }
+        }
+    }
+    return array;
+}
+
+System.Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of array element: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of array element: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2DArray(rows, columns, minValue, maxValue);
+Print2DArray(myArray);
+Print2DArray(Ordering2DArray(myArray));
+
